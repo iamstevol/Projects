6d3af53fd540327076d7fe1fa98a222394376a97
iamstevol <94203327+iamstevol@users.noreply.github.com>
2021-11-15 05:18:26 +0100

    Add files via upload
    Student achievement verifier which automatically classify student score as either pass or fail.

---
+++ student achievement.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+
+#side hustle capstone project
+
+percentage = 70
+average = 60
+student_percentage_score = float(input("Write your percentage score: "))
+
+if (student_percentage_score >= average) and (student_percentage_score > average -5):
+    print ("pass")
+else:
+    print ("fail)")
+
